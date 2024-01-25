1bc8d11d75d89c2c4ed85eaf190ed735dfe235bd
michelle-yuan <156224527+michelle-yuan@users.noreply.github.com>
2024-01-25 17:43:51 +0000

    quadrant complete

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
-  public static void Main(String[] args){
-    System.out.print("Hello Ravens!");
+  public static void main(String[] args){
+    System.out.println("Hello Ravens!");
+    System.out.print("Quadrant number: " + MyStaticMethods.quadrant(1, -1));
   }
 }
---
+++ MyStaticMethods.java
@@ -0,0 +1,20 @@
+public class MyStaticMethods {
+    public static int quadrant (double x, double y){
+        //double x, y;
+        if (x > 0 && y > 0){
+            return 1;
+        }
+        else if (x < 0 && y > 0){
+            return 2;
+        }
+        else if (x < 0 && y < 0){
+            return 3;
+        }
+        else if (x > 0 && y < 0){
+            return 4;
+        }
+        else {
+            return 0;
+        }
+}
+}
